c865780668a5e90277f756aa4619bc1bf3b14df2
Gabriele Facciolo <gfacciol@gmail.com>
2022-09-04 23:29:23 +0200

    0

--- tsne-demo.py
+++ tsne-demo.py
@@ -1,12 +1,8 @@
 
-#%% Markdown [ ]:
-
-# # t-SNE
+# # t-SNE demo
 
 import sys
 
-
-#%% In [ ]:
 print(sys.argv)
 
 bindir=sys.argv[1]
@@ -18,13 +14,11 @@ tsne_perplexity=float(sys.argv[6])
 tsne_iter=int(sys.argv[7])
 
 preprocess_data_with_pca=True if preprocess_data=='pca' else False
-#%% Markdown [ ]:
 
 # # Import packages
 
 
 
-#%% In [ ]:
 
 import numpy as np
 import pandas as pd
@@ -36,14 +30,13 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
 import time
-#%% Markdown [ ]:
 
 # # Load Data and copy it into a dataframe
 
 
 
-#%% In [ ]:
 
+# faster alternative to fetch_openml
 #mnist = fetch_openml(dataset)
 d = np.load('%s/%s.npz'%(bindir,dataset), allow_pickle=True)
 X = d['data'] / 255.0
@@ -54,25 +47,19 @@ print(X.shape, y.shape)
 print(type(X), type(y))
 
 
-#%% In [ ]:
-
 df = pd.DataFrame(X)
 
 
-#%% In [ ]:
 
 df['y'] = y
 df['label'] = df['y'].apply(lambda i: str(i))
 
 X, y = None, None
 print('Size of the dataframe: {}'.format(df.shape))
-#%% Markdown [ ]:
 
 # # Sample random 10000 MNIST data
 
 
-
-#%% In [ ]:
 
 # For reproducability of the results
 np.random.seed(42)
@@ -80,71 +67,50 @@ rndperm = np.random.permutation(df.shape[0])
 
 
 
-#%% In [ ]:
-
 N = data_sample_size
 df_subset = df.loc[rndperm[:N],:].copy()
 data_subset = df_subset.iloc[:,:Xshape[1]].values
 
 
-#%% In [ ]:
-
 data_subset.shape
-#%% Markdown [ ]:
-
-# # PCA
 
 
+# # PCA and tsne
 
-#%% In [ ]:
+
 
 pca2 = PCA(n_components=n_pca_components)
-#%% Markdown [ ]:
 
-# # tsne
-
-
-
-#%% In [ ]:
 
 tsne = TSNE(n_components=2, perplexity=tsne_perplexity, n_iter=tsne_iter)
-#%% Markdown [ ]:
 
-# # Process data
-
-
-
-#%% In [ ]:
+# run pca
 
 time_start = time.time()
-pca_2_result = pca2.fit_transform(data_subset)
-time_taken_PCA2 = time.time()-time_start
-print('PCA done! Time elapsed: {} seconds'.format(time_taken_PCA2))
+pca2_result = pca2.fit_transform(data_subset)
+time_taken_PCA = time.time()-time_start
+print('PCA done! Time elapsed: {} seconds'.format(time_taken_PCA))
 
 
-#%% In [ ]:
-
-df_subset['tsne-PCA2-x'] = pca_2_result[:,0]
-df_subset['tsne-PCA2-y'] = pca_2_result[:,1]
+df_subset['PCA2-x'] = pca2_result[:,0]
+df_subset['PCA2-y'] = pca2_result[:,1]
 
 
-#%% In [ ]:
+
 
 plt.figure(figsize=(8,6))
 sns.scatterplot(
-    x="tsne-PCA2-x", y="tsne-PCA2-y",
+    x="PCA2-x", y="PCA2-y",
     hue="y",
     palette=sns.color_palette("hls", 10),
     data=df_subset,
     legend="full",
     alpha=0.3
-).set(title='MNIST PCA 2 ({})'.format(time_taken_PCA2))
+).set(title='PCA({})'.format(time_taken_PCA))
 
 
-#%% In [ ]:
 
 plt.savefig('pca.png')
-#%% Markdown [ ]:
 
 # # run now run tsne on top
 
@@ -152,39 +118,35 @@ plt.savefig('pca.png')
 if preprocess_data_with_pca:
     pass
 else:
-    pca_2_result=data_subset
+    pca2_result=data_subset
 
-#%% In [ ]:
+# run tsne
 
 time_start = time.time()
-tsne_results = tsne.fit_transform(pca_2_result)
-time_taken_PCA2 = time.time()-time_start
-print('t-SNE done! Time elapsed: {} seconds'.format(time_taken_PCA2))
+tsne_results = tsne.fit_transform(pca2_result)
+time_taken_tsne = time.time()-time_start
+print('t-SNE done! Time elapsed: {} seconds'.format(time_taken_tsne))
 
 
-#%% In [ ]:
 
-df_subset['tsne-PCA10-x'] = tsne_results[:,0]
-df_subset['tsne-PCA10-y'] = tsne_results[:,1]
+df_subset['tsne-x'] = tsne_results[:,0]
+df_subset['tsne-y'] = tsne_results[:,1]
 
 
-#%% In [ ]:
 
 plt.figure(figsize=(8,6))
 sns.scatterplot(
-    x="tsne-PCA10-x", y="tsne-PCA10-y",
+    x="tsne-x", y="tsne-y",
     hue="y",
     palette=sns.color_palette("hls", 10),
     data=df_subset,
     legend="full",
     alpha=0.3
-).set(title='PCA 10 t-SNE ({})'.format(time_taken_PCA2))
+).set(title='t-SNE ({})'.format(time_taken_tsne))
 
 
-#%% In [ ]:
 
 plt.savefig('tsne.png')
 
 
-#%% In [ ]:
 
