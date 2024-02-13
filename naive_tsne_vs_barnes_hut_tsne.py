e092fb0e415ef2c8a3ce1dd7d1eaa182a3d0c192
Tristan Dagobert <tristan.dagobert@cmla.ens-cachan.fr>
2024-02-13 23:04:36 +0100

    option tif+map

--- naive_tsne_vs_barnes_hut_tsne.py
+++ naive_tsne_vs_barnes_hut_tsne.py
@@ -43,6 +43,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import iio
 import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
@@ -51,6 +52,62 @@ from sklearn.datasets import fetch_openml
 import time
 
 
+def write_image_with_colors(cfg, img):
+    """
+    …
+    """
+    
+    palette = sns.color_palette("hls", 10)
+    # select the class
+    y = img[:, :, 0]
+    class_values = np.unique(y)
+    colors = sns.color_palette("hls", 10)
+    mappe = np.zeros((y.shape[0], y.shape[1], 3))
+
+    # write the map
+    for i in np.arange(class_values.shape[0]):
+        mappe[y == class_values[i]] = colors[i]
+    mappe = np.array(255.0 * mappe, dtype=np.uint8)
+    iio.write(cfg.map, mappe)
+    
+    # select the assumed RGB channels
+    rgb = img[:, :, 1:4]
+    # normalisation
+    mini = np.min(rgb)
+    maxi = np.max(rgb)
+    rgb = 255 * (rgb - mini) / (maxi - mini)
+    rgb = np.array(rgb, dtype=np.uint8)
+    iio.write(cfg.rgb, rgb)
+    return
+
+
+def tif_data_loading(cfg):
+    """
+    Loads data stored as a TIF image denoted im, where the class is the first
+    channel : im.shape = (nrow, ncol, ncan). Could be useful for multi-spectral
+    data.
+    """
+    img = iio.read(cfg.tif)
+    datum = img.reshape(img.shape[0] * img.shape[1], img.shape[2])
+    X = datum[:, 1:]
+    y = datum[:, 0]
+
+    feat_cols = ["pixel" + str(i) for i in range(X.shape[1])]
+    df = pd.DataFrame(X, columns=feat_cols)
+    df['y'] = y
+    df['label'] = df['y'].apply(lambda i: str(i))
+    df['pixel0'] = 0.0    
+    
+    np.random.seed(42)  # For reproducability of the results
+    rndperm = np.random.permutation(df.shape[0])
+
+    N = cfg.ndata
+    df_subset = df.loc[rndperm[:N], :].copy()
+    data_subset = df_subset[feat_cols].values
+    
+    return data_subset, df_subset, img
+
+    
 def csv_data_loading(cfg):
     """
     Loads data provided in CSV format.
@@ -140,13 +197,18 @@ def apply_tsne(cfg):
     Load data, compute the TSNE clustering and plot the results
     """
 
-    # loading the MNIST dataset
+    # loading the dataset
     if cfg.dataname == "mnist":
         data, df_subset = mnist_data_loading(cfg)
     elif cfg.dataname == "cifar10":
         data, df_subset = cifar_data_loading(cfg)
     elif cfg.dataname == "csv":
         data, df_subset = csv_data_loading(cfg)
+    elif cfg.dataname == "tif":
+        data, df_subset, img = tif_data_loading(cfg)
+        # plot the image with the encoded colors
+        write_image_with_colors(cfg, img)
+        exit()
     if cfg.pca != 0:
         pca_transform = PCA(n_components=cfg.pca)
         print(data)
@@ -205,10 +267,11 @@ def main():
 
     a_parser = subparsers.add_parser("tsne", help="Naive TSNE.")
     a_parser.add_argument("--ndata", type=int, required=True,
-                          help="Size of the MNIST subset.")
+                          help="Size of the sample.")
     a_parser.add_argument(
-        "--dataname", type=str, required=True, choices=["mnist", "cifar10", "csv"],
-        help="Type of dataset among CIFAR 10 and MNIST."
+        "--dataname", type=str, required=True,
+        choices=["mnist", "cifar10", "csv", "tif"],
+        help="Type of dataset among CIFAR 10, MNIST or CSV, TIF format."
     )
     a_parser.add_argument("--method", type=str, required=True,
                           help="Figure name.", choices=["barnes", "naive"])
@@ -222,6 +285,12 @@ def main():
                           help="PCA components.", default=0)
     a_parser.add_argument("--csv", type=str, required=False,
                           help="CSV filename.")
+    a_parser.add_argument("--tif", type=str, required=False,
+                          help="TIF filename.")
+    a_parser.add_argument("--map", type=str, required=False,
+                          help="Output class filename.")
+    a_parser.add_argument("--rgb", type=str, required=False,
+                          help="RGB output image.")
     
     
     cfg = parser.parse_args()
